ceba8c4e16a32de605e55f3c061fae25e164ba9a
KubraKilic <KubraKilic@DESKTOP-5SLI20L>
2021-02-11 20:47:41 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> customers = new MyDictionary<int, string>();
+            customers.Add(34,"Kübra");
+            customers.Add(45,"Büşra");
+            customers.Add(55,"Leyla");
+            customers.Add(87,"Ozan");
+
+            customers.ToList();
+        }
+    }
+}
